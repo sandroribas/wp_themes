a29b1576e0deb30075514ecf86c9bde632dd9ca3
sandroribas <sandroribas@hotmail.com>
2012-10-17 23:46:24 -0300

    nova versao do site(copia do wordpress 2br)

--- sidebar.php
+++ sidebar.php
@@ -1,6 +1,8 @@
 <div id="barra-lateral">
     <?php get_search_form('minha_busca'); ?>
+    <div id="widgets-side">
     <?php if ( !function_exists('dynamic_sidebar') || !dynamic_sidebar('Home A') ) : ?>
         			<p><?php _e('Esta coluna é uma widget area. '); ?><?php _e('Adicione widgets no '); ?><strong><?php _e('Home A'); ?></strong> <?php _e('para mais funcionalidades!'); ?></p>
+	</div>
 	<?php endif; ?>
 </div>
